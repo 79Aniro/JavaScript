24c8c3c48ff076b1dcc5e624c80393ef8338e8f4
79Aniro <capablanca1@bol.com.br>
2019-03-15 11:18:05 -0300

    Operadores,If e Else

---
+++ JAVASCRIPT, JQUERY, AJAX e JSON Aprendendo a partir do ZERO/Operador Ternario-If e Else/Praticas/JAVASCRIPT/scripts/main.js
@@ -0,0 +1,21 @@
+var a=500;
+var b=300;
+var c;
+
+alert(a>b ? "A é maior que B": "A não é maior que B");
+
+
+//[expressao] ? condicao 1: condicao2;
+
+if(a>b){
+	
+	alert("A é maior que B");
+}
+else if(a==b){
+	alert("A é igual a B");
+}
+
+else{
+	
+	alert("B é maior que A");
+}
---
+++ JAVASCRIPT, JQUERY, AJAX e JSON Aprendendo a partir do ZERO/Switch case/Praticas/JAVASCRIPT/scripts/main.js
@@ -0,0 +1,20 @@
+var a=200;
+
+
+switch(a){
+	
+	case 10:
+	alert("Entrou no 10");
+	break;
+	case 20:
+	alert("Entrou no 20");
+	break;
+	case 30:
+	alert("Entrou no 30");
+	case 40:
+	alert("Entrou no 40");
+	break;
+	default:
+	alert("Não encontrou o numero");
+	break;
+}
